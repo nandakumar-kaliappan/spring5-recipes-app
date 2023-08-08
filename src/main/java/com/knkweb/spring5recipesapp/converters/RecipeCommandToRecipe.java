8f68a2d7013f673d38f3dcfcefd43d2139a07d30
nandakumar-kaliappan <nandakumarkalliappan@gmail.com>
2023-08-08 19:43:57 +0530

    Recipe convertion completed and tested

--- src/main/java/com/knkweb/spring5recipesapp/converters/RecipeCommandToRecipe.java
+++ src/main/java/com/knkweb/spring5recipesapp/converters/RecipeCommandToRecipe.java
@@ -1,9 +1,17 @@
 package com.knkweb.spring5recipesapp.converters;
 
 import com.knkweb.spring5recipesapp.commands.RecipeCommand;
+import com.knkweb.spring5recipesapp.domain.Category;
+import com.knkweb.spring5recipesapp.domain.Ingredient;
+import com.knkweb.spring5recipesapp.domain.Notes;
 import com.knkweb.spring5recipesapp.domain.Recipe;
+import lombok.Synchronized;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 public class RecipeCommandToRecipe implements Converter<RecipeCommand, Recipe> {
@@ -20,9 +28,44 @@ public class RecipeCommandToRecipe implements Converter<RecipeCommand, Recipe> {
     }
 
 
-
+    @Synchronized
+    @Nullable
     @Override
     public Recipe convert(RecipeCommand source) {
-        return null;
+        if(source == null){
+            return null;
+        }
+        Notes notes = notesCommandToNotes.convert(source.getNotes());
+
+
+        Recipe recipe = Recipe.builder()
+                .id(source.getId())
+                .description(source.getDescription())
+                .directions(source.getDirections())
+                .prepTime(source.getPrepTime())
+                .cookTime(source.getCookTime())
+                .servings(source.getServings())
+                .source(source.getSource())
+                .url(source.getUrl())
+                .difficulty(source.getDifficulty())
+                .notes(notes)
+                .build();
+        if (source.getIngredients() != null && source.getIngredients().size() > 0) {
+            Set<Ingredient> ingredients = new HashSet<>();
+            source.getIngredients().stream().forEach(ingredientCommand ->
+                    ingredients.add(ingredientCommandToIngredient.convert(ingredientCommand)));
+            recipe.setIngredients(ingredients);
+        }
+
+
+        if (source.getCategories() != null && source.getCategories().size() > 0) {
+            Set<Category> categories = new HashSet<>();
+            source.getCategories().stream().forEach(categoryCommand ->
+                    categories.add(categoryCommandToCategory.convert(categoryCommand)));
+            recipe.setCategories(categories);
+        }
+        return recipe;
+
+
     }
 }
--- src/main/java/com/knkweb/spring5recipesapp/converters/RecipeToRecipeCommand.java
+++ src/main/java/com/knkweb/spring5recipesapp/converters/RecipeToRecipeCommand.java
@@ -1,8 +1,11 @@
 package com.knkweb.spring5recipesapp.converters;
 
 import com.knkweb.spring5recipesapp.commands.RecipeCommand;
+import com.knkweb.spring5recipesapp.domain.Category;
 import com.knkweb.spring5recipesapp.domain.Recipe;
+import lombok.Synchronized;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,9 +22,37 @@ public class RecipeToRecipeCommand implements Converter<Recipe, RecipeCommand> {
         this.ingredientToIngredientCommand = ingredientToIngredientCommand;
         this.categoryToCategoryCommand = categoryToCategoryCommand;
     }
-
+    @Synchronized
+    @Nullable
     @Override
     public RecipeCommand convert(Recipe source) {
-        return null;
+
+        if (source == null) {
+            return null;
+        }
+
+        final RecipeCommand command = new RecipeCommand();
+        command.setId(source.getId());
+        command.setCookTime(source.getCookTime());
+        command.setPrepTime(source.getPrepTime());
+        command.setDescription(source.getDescription());
+        command.setDifficulty(source.getDifficulty());
+        command.setDirections(source.getDirections());
+        command.setServings(source.getServings());
+        command.setSource(source.getSource());
+        command.setUrl(source.getUrl());
+        command.setNotes(notesToNotesCommand.convert(source.getNotes()));
+
+        if (source.getCategories() != null && source.getCategories().size() > 0){
+            source.getCategories()
+                    .forEach((Category category) -> command.getCategories().add(categoryToCategoryCommand.convert(category)));
+        }
+
+        if (source.getIngredients() != null && source.getIngredients().size() > 0){
+            source.getIngredients()
+                    .forEach(ingredient -> command.getIngredients().add(ingredientToIngredientCommand.convert(ingredient)));
+        }
+
+        return command;
     }
 }
