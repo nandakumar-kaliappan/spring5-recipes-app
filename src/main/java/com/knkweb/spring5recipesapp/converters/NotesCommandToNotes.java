75425afe53179c6b92a9f9e13759778b9d871b09
nandakumar-kaliappan <nandakumarkalliappan@gmail.com>
2023-08-08 19:15:23 +0530

    Notes convertion completed and tested

--- src/main/java/com/knkweb/spring5recipesapp/converters/NotesCommandToNotes.java
+++ src/main/java/com/knkweb/spring5recipesapp/converters/NotesCommandToNotes.java
@@ -2,13 +2,24 @@ package com.knkweb.spring5recipesapp.converters;
 
 import com.knkweb.spring5recipesapp.commands.NotesCommand;
 import com.knkweb.spring5recipesapp.domain.Notes;
+import lombok.Synchronized;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NotesCommandToNotes implements Converter<NotesCommand, Notes> {
+    @Synchronized
+    @Nullable
     @Override
     public Notes convert(NotesCommand source) {
-        return null;
+        if(source == null) {
+            return null;
+        }
+
+        final Notes notes = new Notes();
+        notes.setId(source.getId());
+        notes.setRecipeNotes(source.getRecipeNotes());
+        return notes;
     }
 }
--- src/main/java/com/knkweb/spring5recipesapp/converters/NotesToNotesCommand.java
+++ src/main/java/com/knkweb/spring5recipesapp/converters/NotesToNotesCommand.java
@@ -2,13 +2,24 @@ package com.knkweb.spring5recipesapp.converters;
 
 import com.knkweb.spring5recipesapp.commands.NotesCommand;
 import com.knkweb.spring5recipesapp.domain.Notes;
+import lombok.Synchronized;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NotesToNotesCommand implements Converter<Notes, NotesCommand> {
+    @Synchronized
+    @Nullable
     @Override
     public NotesCommand convert(Notes source) {
-        return null;
+        if (source == null) {
+            return null;
+        }
+
+        final NotesCommand notesCommand = new NotesCommand();
+        notesCommand.setId(source.getId());
+        notesCommand.setRecipeNotes(source.getRecipeNotes());
+        return notesCommand;
     }
 }
